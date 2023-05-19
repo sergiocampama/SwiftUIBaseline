1b0249614bd32b2473dc4a73b413b80a8ce8dde0
Sergio Campamá <kaipi@copilot.money>
2023-05-19 16:59:46 -0400

    baseline issue

--- SwiftUIBaseline/ViewController.swift
+++ SwiftUIBaseline/ViewController.swift
@@ -1,19 +1,30 @@
-//
-//  ViewController.swift
-//  SwiftUIBaseline
-//
-//  Created by Sergio Campama on 19/5/23.
-//
-
 import UIKit
 
+#if IMPORT_SWIFT_UI
+import SwiftUI
+#endif
+
 class ViewController: UIViewController {
+  let label = UILabel()
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+
+    view.addSubview(label)
+
+    label.backgroundColor = .red.withAlphaComponent(0.3)
+
+    var attributedString = try! AttributedString(markdown: "BaselineTest")
+    attributedString.baselineOffset = 100
+
+    label.attributedText = NSAttributedString(attributedString)
   }
 
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
 
+    label.frame.size = CGSize(width: 200, height: 200)
+    label.center = view.center
+  }
 }
 
